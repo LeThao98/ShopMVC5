442f5cb8182ada62fbe2de7a4e5c94b75202a95f
LeThao98 <ledinhthao131098@gmail.com>
2019-07-18 15:52:48 +0700

    Add domain entities for project.

---
+++ TeduShop.Model/Abstracts/IAuditable.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeduShop.Model.Abstracts
+{
+    public interface IAuditable
+    {
+        DateTime? CreatedTime { get; set; }
+        string CreatedBy { get; set; }
+        DateTime? UpdatedTime { get; set; }
+        string UpdatedBy { get; set; }
+    }
+}
